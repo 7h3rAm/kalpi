eebd2b6b18103409a3e22e450fdd8cf9b42a1081
Ankur Tyagi (7h3rAm) <7h3rAm@gmail.com>
2015-12-20 14:20:45 +0530

    Added related_posts generation logic via two strategies: tags_date and tags_random.

--- kalpi.py
+++ kalpi.py
@@ -9,6 +9,7 @@ import re
 import sys
 import time
 import errno
+import random
 import shutil
 import datetime
 from pprint import pprint
@@ -17,7 +18,7 @@ try:
   import jinja2
   import markdown2 as markdown
 except ImportError as error:
-  print 'ImportError:', str(error)
+  print "ImportError:", str(error)
   exit(1)
 
 
@@ -78,19 +79,19 @@ class kalpi:
   # 4. pandoc: md->tex->pdf
   # 5. related posts (top 3 by tags)
   # 6. move static templates to yaml
-  # 7. latex cv; last updated; typeset: xelatex
 
   def __init__(self):
     self.baseurl = "http://7h3ram.github.io/"
-
-    self.basepath = os.path.dirname(os.path.realpath(__file__))
+    self.baseurl = "/"
     self.basepath = "/home/shiv/toolbox/7h3rAm.github.com"
-
     self.blogdir = "%s/" % (self.basepath)
     self.postsdir = "%s/_posts/" % (self.basepath)
     self.templatedir = "%s/_templates/" % (self.basepath)
     self.staticdir = "%s/_static/" % (self.basepath)
     self.homepostscount = 5
+    self.relatedpostscount = 3
+    self.relatedpostsstrategy = "tags_date"
+    self.relatedpostsstrategy = "tags_random"
     self.templateopts = dict()
 
     self.urlextension = ".html"
@@ -106,7 +107,7 @@ class kalpi:
     self.rssdateformat = "%a, %d %b %Y %H:%M:%S %z"
     self.postdateformat = "%d/%b/%Y"
 
-    self.format = lambda text: markdown.markdown(text, extras=['fenced-code-blocks','footnotes', 'metadata', 'smarty-pants', 'tables'])
+    self.format = lambda text: markdown.markdown(text, extras=["fenced-code-blocks","footnotes", "metadata", "smarty-pants", "tables"])
 
   def parse(self, lines):
     date, summary, tags, content = None, None, None, None
@@ -120,37 +121,61 @@ class kalpi:
         for tag in "".join(line.split(":")[1:]).strip().split(", "):
           tags.append(tag.replace(" ", "_"))
       if line == "\n":
-        content = self.format(''.join(lines[idx+1:]).decode('UTF-8'))
+        content = self.format("".join(lines[idx+1:]).decode("UTF-8"))
         break
     return date, summary, tags, content
 
   def get_tree(self, source):
     files = list()
+    self.tagsinfo = dict()
     for root, ds, fs in os.walk(source):
       for name in fs:
         if name[0] == ".": continue
-        if not re.match(r'^.+\.(md|mdown|markdown)$', name): continue
+        if not re.match(r"^.+\.(md|mdown|markdown)$", name): continue
         path = os.path.join(root, name)
         with open(path, "rU") as f:
-          title = f.readline().decode('UTF-8')[:-1].strip('\n..')
+          title = f.readline().decode("UTF-8")[:-1].strip("\n..")
           date, summary, tags, content = self.parse(f.readlines())
           year, month, day = date[:3]
-          url = '/'.join([str(year), os.path.splitext(name)[0] + self.urlextension])
+          pretty_date = time.strftime(self.timeformat, date)
+          epoch = time.mktime(date)
+          url = "/".join([str(year), os.path.splitext(name)[0] + self.urlextension])
           files.append({
-            'title': title,
-            'epoch': time.mktime(date),
-            'content': content,
-            'url': url,
-            'pretty_date': time.strftime(self.timeformat, date),
-            'sdate': time.strftime(self.stimeformat, date),
-            'rssdate': time.strftime(self.rssdateformat, date),
-            'date': date,
-            'year': year,
-            'month': month,
-            'day': day,
-            'tags': tags,
-            'summary': summary,
-            'filename': name})
+            "title": title,
+            "epoch": epoch,
+            "content": content,
+            "url": url,
+            "pretty_date": pretty_date,
+            "sdate": time.strftime(self.stimeformat, date),
+            "rssdate": time.strftime(self.rssdateformat, date),
+            "date": date,
+            "year": year,
+            "month": month,
+            "day": day,
+            "tags": tags,
+            "summary": summary,
+            "filename": name})
+
+          for tag in tags:
+            if self.relatedpostsstrategy == "tags_date":
+              if tag not in self.tagsinfo.keys():
+                self.tagsinfo[tag] = dict({
+                  epoch: [(title, url, pretty_date)]
+                })
+              elif epoch not in self.tagsinfo[tag].keys():
+                self.tagsinfo[tag][epoch] = [(title, url, pretty_date)]
+              else:
+                self.tagsinfo[tag][epoch].append((title, url, pretty_date))
+
+            elif self.relatedpostsstrategy == "tags_random":
+              if tag not in self.tagsinfo.keys():
+                self.tagsinfo[tag] = list([(title, url, pretty_date)])
+              else:
+                self.tagsinfo[tag].append((title, url, pretty_date))
+
+            else:
+              pass
+
     return files
 
   def write_file(self, url, data):
@@ -158,82 +183,106 @@ class kalpi:
     dirs = os.path.dirname(path)
     if not os.path.isdir(dirs): os.makedirs(dirs)
     with open(path, "w") as file:
-      file.write(data.encode('UTF-8'))
+      file.write(data.encode("UTF-8"))
 
   def write_feed(self, url, data):
     path = self.blogdir + url
     with open(path, "w") as file:
-      file.write(data.encode('UTF-8'))
+      file.write(data.encode("UTF-8"))
 
   def write_sitemap(self, url, data):
     path = self.blogdir + url
     with open(path, "wb") as file:
-      file.write(data.encode('UTF-8'))
+      file.write(data.encode("UTF-8"))
 
   def do_static(self):
     utils().copy_dir(self.staticdir, "%s/static/" % (self.blogdir))
 
-  def gen_posts(self, f, e, d):
-    for file in f:
-      self.write_file(file['url'], e.get_template('post.html').render(post=file, posts=f, baseurl=self.baseurl, date=d))
+  def gen_posts(self):
+    for f in self.files:
+      related_posts = list()
+      if self.relatedpostsstrategy == "tags_date":
+        rlposts = dict()
+        for tag in f["tags"]:
+          for epoch in self.tagsinfo[tag]:
+            if self.tagsinfo[tag][epoch][0][0] == f["title"]:
+              continue
+            rlposts[epoch] = self.tagsinfo[tag][epoch][0]
 
-  def gen_index(self, f, e, d):
-    self.write_file('index' + self.urlextension, e.get_template('index.html').render(posts=f[:self.homepostscount], baseurl=self.baseurl, date=d))
+        for epoch in sorted(rlposts, reverse=True):
+          related_posts.append(rlposts[epoch])
 
-  def gen_archive(self, f, e, d):
-    self.write_file('archive' + self.urlextension, e.get_template('archive.html').render(posts=f, baseurl=self.baseurl, date=d))
+      elif self.relatedpostsstrategy == "tags_random":
+        rlposts = list()
+        for tag in f["tags"]:
+          for tag in self.tagsinfo[tag]:
+            rlposts.append(tag)
 
-  def gen_tags(self, f, e, d):
-    tags = dict({})
-    for file in f:
+        random.shuffle(rlposts)
+        for tag in rlposts:
+          if tag[0] == f["title"]:
+            continue
+          if tag not in related_posts:
+            related_posts.append(tag)
+
+      self.write_file(f["url"], self.env.get_template("post.html").render(post=f, posts=self.files, related_posts=related_posts[:self.relatedpostscount], baseurl=self.baseurl, date=self.date))
+
+  def gen_index(self):
+    self.write_file("index%s" % self.urlextension, self.env.get_template("index.html").render(posts=self.files[:self.homepostscount], baseurl=self.baseurl, date=self.date))
+
+  def gen_archive(self):
+    self.write_file("archive%s" % self.urlextension, self.env.get_template("archive.html").render(posts=self.files, baseurl=self.baseurl, date=self.date))
+
+  def gen_tags(self):
+    self.tags = dict({})
+    for file in self.files:
       for tag in file["tags"]:
-        if tag not in tags.keys():
-          tags[tag] = dict({
+        if tag not in self.tags.keys():
+          self.tags[tag] = dict({
             "count": 0,
             "files": [file]
           })
         else:
-          tags[tag]["count"] += 1
-          tags[tag]["files"].append(file)
-    self.write_file('tags' + self.urlextension, e.get_template('tags.html').render(tags=tags, baseurl=self.baseurl, date=d))
+          self.tags[tag]["count"] += 1
+          self.tags[tag]["files"].append(file)
+    self.write_file("tags%s" % self.urlextension, self.env.get_template("tags.html").render(tags=self.tags, baseurl=self.baseurl, date=self.date))
 
-  def gen_code(self, f, e, d):
-    self.write_file('code' + self.urlextension, e.get_template('code.html').render(posts=f, baseurl=self.baseurl, date=d))
+  def gen_code(self):
+    self.write_file("code%s" % self.urlextension, self.env.get_template("code.html").render(posts=self.files, baseurl=self.baseurl, date=self.date))
 
-  def gen_talks(self, f, e, d):
-    self.write_file('talks' + self.urlextension, e.get_template('talks.html').render(posts=f, baseurl=self.baseurl, date=d))
+  def gen_talks(self):
+    self.write_file("talks%s" % self.urlextension, self.env.get_template("talks.html").render(posts=self.files, baseurl=self.baseurl, date=self.date))
 
-  def gen_cv(self, f, e, d):
-    self.write_file('cv' + self.urlextension, e.get_template('cv.html').render(posts=f, baseurl=self.baseurl, date=d))
+  def gen_cv(self):
+    self.write_file("cv%s" % self.urlextension, self.env.get_template("cv.html").render(posts=self.files, baseurl=self.baseurl, date=self.date))
 
-  def gen_about(self, f, e, d):
-    self.write_file('about' + self.urlextension, e.get_template('about.html').render(post=file, baseurl=self.baseurl, date=d))
+  def gen_about(self):
+    self.write_file("about%s" % self.urlextension, self.env.get_template("about.html").render(baseurl=self.baseurl, date=self.date))
 
-  def gen_rss(self, f, e, d):
-    self.write_feed('rss.xml', e.get_template('rss.html').render(posts=f[:self.homepostscount], baseurl=self.baseurl, date=d))
+  def gen_rss(self):
+    self.write_feed("rss.xml", self.env.get_template("rss.html").render(posts=self.files[:self.homepostscount], baseurl=self.baseurl, date=self.date))
 
-  def gen_sitemap(self, f, e, d):
-    self.write_sitemap('sitemap.xml', e.get_template('sitemap.html').render(posts=f, baseurl=self.baseurl, date=d))
+  def gen_sitemap(self):
+    self.write_sitemap("sitemap.xml", self.env.get_template("sitemap.html").render(posts=self.files, baseurl=self.baseurl, date=self.date))
 
   def create(self):
-    date = utils().get_current_date()
+    self.date = utils().get_current_date()
+    self.files = sorted(self.get_tree(self.postsdir), key=lambda post: post["epoch"], reverse=True)
+    self.env = jinja2.Environment(loader=jinja2.FileSystemLoader(self.templatedir), extensions=["jinja2_markdown.MarkdownExtension"], **self.templateopts)
+    self.env.filters["monthname"] = utils().get_month_name
+    self.env.filters["joinlistand"] = utils().join_list_and
+
     self.do_static()
-
-    files = sorted(self.get_tree(self.postsdir), key=lambda post: post['epoch'], reverse=True)
-    env = jinja2.Environment(loader=jinja2.FileSystemLoader(self.templatedir), extensions=['jinja2_markdown.MarkdownExtension'], **self.templateopts)
-    env.filters['monthname'] = utils().get_month_name
-    env.filters['joinlistand'] = utils().join_list_and
-
-    self.gen_posts(files, env, date)
-    self.gen_index(files, env, date)
-    self.gen_archive(files, env, date)
-    self.gen_tags(files, env, date)
-    self.gen_code(files, env, date)
-    self.gen_talks(files, env, date)
-    self.gen_cv(files, env, date)
-    self.gen_about(files, env, date)
-    self.gen_rss(files, env, date)
-    self.gen_sitemap(files, env, date)
+    self.gen_posts()
+    self.gen_index()
+    self.gen_archive()
+    self.gen_tags()
+    self.gen_code()
+    self.gen_talks()
+    self.gen_cv()
+    self.gen_about()
+    self.gen_rss()
+    self.gen_sitemap()
 
 
 if __name__ == "__main__":
