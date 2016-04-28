8341c300225ecd3fea8c1b8d8a355f52348403a8
Ankur Tyagi (7h3rAm) <7h3rAm@gmail.com>
2016-04-28 17:15:35 +0530

    Added stats calculation for dashboard creation and removed PDF post files creation code.

--- kalpi.py
+++ kalpi.py
@@ -85,18 +85,28 @@ class kalpi:
   def __init__(self):
     self.baseurl = "http://7h3ram.github.io/"
     self.baseurl = "/"
-    self.basepath = "/home/shiv/toolbox/7h3rAm.github.com"
+    self.basepath = "/home/shiv/toolbox/7h3rAm.github.io"
     self.blogdir = "%s/" % (self.basepath)
     self.postsdir = "%s/_posts/" % (self.basepath)
     self.templatedir = "%s/_templates/" % (self.basepath)
-    self.staticdir = "%s/_static/" % (self.basepath)
+    self.staticdir = "%s/static/" % (self.basepath)
     self.homepostscount = 5
     self.relatedpostscount = 3
     self.relatedpostsstrategy = "tags_date"
     self.relatedpostsstrategy = "tags_random"
     self.templateopts = dict()
     self.pdf = False
-    self.mdbaseurl = "https://raw.githubusercontent.com/7h3rAm/7h3rAm.github.io/master/_posts/"
+    self.mdbaseurl = "https://raw.githubusercontent.io/7h3rAm/7h3rAm.github.io/master/_posts/"
+    self.tags = dict()
+    self.posts = dict()
+    self.stats = dict({
+      "count_posts": 0,
+      "count_tags": 0,
+      "count_words": 0,
+      "count_readtime": 0,
+      "chart_data": dict()
+    })
+    self.chartdatafile = "%s/files/chart-data.js" % (self.staticdir)
 
     self.urlextension = ".html"
     if self.urlextension == ".html":
@@ -149,15 +159,19 @@ class kalpi:
         break
     return date, summary, tags, content
 
-  def stats(self, title, date, summary, tags, content, wpm=240, avgwordlen=5.1):
+  def poststats(self, title, date, summary, tags, content, wpm=240, avgwordlen=5.1):
     wps = wpm/60
     poststats = dict({
       "title": title,
+      "year": date[:3][0],
+      "month": date[:3][1],
+      "day": date[:3][2],
       "words": 0,
       "characters": 0,
       "tags": 0,
       "images": 0,
-      "readtime": None
+      "readtimestr": None,
+      "readtime": 0
     })
     for word in content.split(" "):
       poststats["characters"] += len(word)
@@ -167,13 +181,18 @@ class kalpi:
 
     # count words via avgwordlength
     poststats["words"] = poststats["characters"]//avgwordlen
+    self.stats["count_words"] += poststats["words"]
 
     t = content.split(" ")
     l = len(t)
     poststats["readtime"] = poststats["words"]/wps
     poststats["readtime"] += 1 if poststats["words"]%wps else 0
-    poststats["readtime"] = self.sec_to_human(poststats["readtime"])
+    poststats["readtimestr"] = self.sec_to_human(poststats["readtime"])
+
+    self.stats["count_readtime"] += poststats["readtime"]
+
     poststats["tags"] = len(tags)
+    poststats["tagslist"] = tags
     poststats["images"] = len(re.findall(r"<img src=", content))
     return poststats
 
@@ -188,11 +207,15 @@ class kalpi:
         with open(path, "rU") as f:
           title = f.readline().decode("UTF-8")[:-1].strip("\n..")
           date, summary, tags, content = self.parse(f.readlines())
-          poststats = self.stats(title, date, summary, tags, content)
+
           year, month, day = date[:3]
           pretty_date = time.strftime(self.timeformat, date)
           epoch = time.mktime(date)
           url = "/".join([str(year), os.path.splitext(name)[0] + self.urlextension])
+
+          poststats = self.poststats(title, date, summary, tags, content)
+          self.posts[name] = poststats
+
           files.append({
             "title": title,
             "epoch": epoch,
@@ -207,7 +230,7 @@ class kalpi:
             "day": day,
             "tags": tags,
             "words": poststats["words"],
-            "readtime": poststats["readtime"],
+            "readtimestr": poststats["readtimestr"],
             "summary": summary,
             "filename": name})
 
@@ -280,7 +303,7 @@ class kalpi:
           if tag not in related_posts:
             related_posts.append(tag)
 
-      self.write_file(url=f["url"], data=self.env.get_template("post.html").render(post=f, posts=self.files, mdbaseurl=self.mdbaseurl, related_posts=related_posts[:self.relatedpostscount], baseurl=self.baseurl, date=self.date), pdfdata=self.env.get_template("post_pdf.html").render(post=f, posts=self.files, mdbaseurl=self.mdbaseurl, related_posts=related_posts[:self.relatedpostscount], baseurl=self.baseurl, date=self.date))
+      self.write_file(url=f["url"], data=self.env.get_template("post.html").render(post=f, posts=self.files, mdbaseurl=self.mdbaseurl, related_posts=related_posts[:self.relatedpostscount], baseurl=self.baseurl, date=self.date))
 
   def gen_index(self):
     self.write_file("index%s" % self.urlextension, self.env.get_template("index.html").render(posts=self.files[:self.homepostscount], baseurl=self.baseurl, date=self.date))
@@ -289,7 +312,6 @@ class kalpi:
     self.write_file("archive%s" % self.urlextension, self.env.get_template("archive.html").render(posts=self.files, baseurl=self.baseurl, date=self.date))
 
   def gen_tags(self):
-    self.tags = dict({})
     maxtagcount = 0
     for file in self.files:
       for tag in file["tags"]:
@@ -334,16 +356,11 @@ class kalpi:
     for tag in tags:
       tagsinfo[tag] = self.tags[tag]
 
+    self.stats["tags"] = self.tags
     self.write_file("tags%s" % self.urlextension, self.env.get_template("tags.html").render(tags=tagsinfo, baseurl=self.baseurl, date=self.date))
 
   def gen_research(self):
     self.write_file("research%s" % self.urlextension, self.env.get_template("research.html").render(posts=self.files, baseurl=self.baseurl, date=self.date))
-
-  def gen_cv(self):
-    self.write_file("cv%s" % self.urlextension, self.env.get_template("cv.html").render(posts=self.files, baseurl=self.baseurl, date=self.date))
-
-  def gen_about(self):
-    self.write_file("about%s" % self.urlextension, self.env.get_template("about.html").render(baseurl=self.baseurl, date=self.date))
 
   def gen_rss(self):
     self.write_feed("rss.xml", self.env.get_template("rss.html").render(posts=self.files[:self.homepostscount], baseurl=self.baseurl, date=self.date))
@@ -352,7 +369,50 @@ class kalpi:
     self.write_sitemap("sitemap.xml", self.env.get_template("sitemap.html").render(posts=self.files, baseurl=self.baseurl, date=self.date))
 
   def gen_dashboard(self):
-    self.write_file("dashboard%s" % self.urlextension, self.env.get_template("dashboard.html").render(posts=self.files, baseurl=self.baseurl, date=self.date))
+    self.stats["count_posts"] = len(self.posts)
+    self.stats["count_tags"] = len(self.tags)
+    self.stats["count_words"] = int(self.stats["count_words"])
+    self.stats["count_readtime"] = int(self.stats["count_readtime"]/60)
+    self.stats["posts_per_tag"] = int(self.stats["count_posts"]/self.stats["count_tags"])
+    self.stats["words_per_post"] = int(self.stats["count_words"]/self.stats["count_posts"])
+    self.stats["words_per_tag"] = int(self.stats["count_words"]/self.stats["count_tags"])
+    self.stats["readtime_per_post"] = int(self.stats["count_readtime"]/self.stats["count_posts"])
+    self.stats["readtime_per_tag"] = int(self.stats["count_readtime"]/self.stats["count_tags"])
+
+    self.stats["chart_data"]["posts_tags"] = list()
+    self.stats["chart_data"]["words_readtime"] = list()
+    years = list()
+    for name, stats in self.posts.iteritems():
+      if stats["year"] not in years:
+        years.append(stats["year"])
+
+    for year in sorted(years):
+      posts, tags, words, readtime = 0, list(), 0, 0
+      for name, stats in self.posts.iteritems():
+        if year == stats["year"]:
+          posts += 1
+          tags += stats["tagslist"]
+          words += (stats["words"]/1000)
+          readtime += (stats["readtime"]/60)
+
+      self.stats["chart_data"]["posts_tags"].append(dict({
+        "y": str(year),
+        "posts": posts,
+        "tags": len(set(tags))
+      }))
+      self.stats["chart_data"]["words_readtime"].append(dict({
+        "y": str(year),
+        "words": int(words),
+        "readtime": int(readtime)
+      }))
+
+    chartdata = """$(function() { Morris.Bar({ element: "yearly-summary-posts-tags", data: %s, xkey: "y", ykeys: ["posts", "tags"], labels: ["Posts #", "Tags #"], hideHover: "auto", resize: true }); Morris.Area({ element: "yearly-summary-words-readtime", data: %s, xkey: "y", ykeys: ["words", "readtime"], labels: ["Words #", "Readtime"], pointSize: 2, hideHover: "auto", resize: true }); });""" % (self.stats["chart_data"]["posts_tags"], self.stats["chart_data"]["words_readtime"])
+    chartdata = chartdata.replace("'", "")
+    chartdata = re.sub(r"y: (\d{4})", "y: '\\1'", chartdata)
+    with open(self.chartdatafile, "w") as fo:
+      fo.write(chartdata)
+
+    self.write_file("dashboard%s" % self.urlextension, self.env.get_template("dashboard.html").render(stats=self.stats, posts=self.files, baseurl=self.baseurl, date=self.date))
 
   def create(self):
     self.date = utils().get_current_date()
@@ -366,7 +426,6 @@ class kalpi:
     self.gen_archive()
     self.gen_tags()
     self.gen_research()
-    self.gen_about()
     self.gen_rss()
     self.gen_sitemap()
     self.gen_dashboard()
